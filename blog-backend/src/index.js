a88cb79bf93b0e8ba49b147c1805bacbcb62370d
elsa-kim <ksh96611@naver.com>
2023-07-02 15:03:12 +0900

    build, koa-static 사용

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -4,6 +4,10 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+import { fileURLToPath } from 'url';
 
 import api from './api/index.js';
 // import createFakeData from './createFakeData.js';
@@ -33,6 +37,15 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
